f7d28b7cd47793a6aa6a600ce1377571450a3684
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-12-02 21:26:34 +0300

    add type annotations to the function

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -2,12 +2,12 @@
 """Duck-typed annotations python"""
 from typing import Union, Mapping, Any, TypeVar
 
-Ty = TypeVar("Ty")
+T = TypeVar("T")
 
 
 def safely_get_value(dct: Mapping, key: Any,
-                     default: Union[Ty, None] = None
-                     ) -> Union[Any, Ty]:
+                     default: Union[T, None] = None
+                     ) -> Union[Any, T]:
     '''correct duck-typed annotations'''
     if key in dct:
         return dct[key]
