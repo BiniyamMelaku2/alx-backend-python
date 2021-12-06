b4db704b569990b8c8ed0af374b05b0505772c13
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-12-06 20:07:42 +0300

    spawn n times wait

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """ asynchronous coroutine waits for a random delay between 0 and max_delay """
 from typing import List
+import asyncio
+import random
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
@@ -9,6 +11,9 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     ''' spawn n times wait for a random delay and returns List'''
     waitList = []
+    spawnList = []
     for i in range(n):
-        waitList += [await wait_random(max_delay)]
-    return waitList
+        waitList.append(wait_random(max_delay))
+    for t in asyncio.as_completed(waitList):
+        spawnList.append(await t)
+    return spawnList
